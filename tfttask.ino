a577292fa7dd40baa46851922620081e58d27e4f
Cellie <m.timmer@wasietsmet.nl>
2017-12-14 13:58:55 +0100

    -Moved html files to folder 'webif'.

--- tfttask.ino
+++ tfttask.ino
@@ -24,21 +24,21 @@ struct button_t
   uint16_t y;
   int16_t  w;
   int16_t  h;
-  char     text[10];
+  char     text[15];
 };
 
 /* buttons used in menu*/
-const button_t LIGHTON_BUTTON
+const button_t LIGHTSON_BUTTON
 {
   10, 10, TFT_BUTTON_WIDTH, TFT_BUTTON_HEIGHT, "ON"
 };
 
-const button_t LIGHTOFF_BUTTON
+const button_t LIGHTSOFF_BUTTON
 {
   10, 90, TFT_BUTTON_WIDTH, TFT_BUTTON_HEIGHT, "OFF"
 };
 
-const button_t LIGHTAUTO_BUTTON
+const button_t LIGHTSAUTO_BUTTON
 {
   10, 170, TFT_BUTTON_WIDTH, TFT_BUTTON_HEIGHT, "AUTO"
 };
@@ -46,12 +46,6 @@ const button_t LIGHTAUTO_BUTTON
 const button_t EXIT_BUTTON
 {
   210, 170, TFT_BUTTON_WIDTH, TFT_BUTTON_HEIGHT, "EXIT"
-};
-
-/* button used on main screen */
-const button_t MENU_BUTTON
-{
-  210, 10, TFT_BUTTON_WIDTH, TFT_BUTTON_HEIGHT, "MENU"
 };
 
 /* slider for backlight control - note: XPOS is center of slider*/
@@ -66,15 +60,21 @@ const button_t sliderArea
   SLIDER_XPOS - SLIDER_WIDTH / 2, SLIDER_YPOS, SLIDER_WIDTH, SLIDER_HEIGHT
 };
 
-bool tftClearScreen = true;
+/* button used on main screen */
+const button_t MENU_BUTTON
+{
+  210, 10, TFT_BUTTON_WIDTH, TFT_BUTTON_HEIGHT, "MENU"
+};
 
 uint16_t backlightMaxvalue;
 
 displayState tftState = normal;
 
+bool tftClearScreen = true;
+
 void tftTask( void * pvParameters )
 {
-  const time_t tftTaskdelayTime         =   ( 1000 / UPDATE_FREQ_TFT) / portTICK_PERIOD_MS;
+  const time_t tftTaskdelayTime = ( 1000 / UPDATE_FREQ_TFT) / portTICK_PERIOD_MS;
 
   tft.fillScreen( TFT_BACK_COLOR );
 
@@ -116,8 +116,8 @@ void showMenu()
     ledcWrite( TFT_BACKLIGHT_CHANNEL, 0 );
     tft.fillScreen( ILI9341_BLACK );
     drawMenuButtons();
-    ledcWrite( TFT_BACKLIGHT_CHANNEL, map( tftBrightness, 0, 100, 0, backlightMaxvalue ) );
     drawBacklightSlider();
+    ledcWrite( TFT_BACKLIGHT_CHANNEL, map( tftBrightness, 0, 100, 0, backlightMaxvalue ) );
     tftClearScreen = false;
   }
 
@@ -125,25 +125,26 @@ void showMenu()
   {
     TS_Point p = touch.getPoint();
 
-    if ( buttonPressed( LIGHTON_BUTTON , p ) )
+    if ( buttonPressed( LIGHTSON_BUTTON , p ) )
     {
       lightsOn();
       drawMenuButtons();
     }
-    else if ( buttonPressed( LIGHTOFF_BUTTON , p ) )
+    else if ( buttonPressed( LIGHTSOFF_BUTTON , p ) )
     {
       lightsOff();
       drawMenuButtons();
     }
-    else if ( buttonPressed( LIGHTAUTO_BUTTON , p ) )
+    else if ( buttonPressed( LIGHTSAUTO_BUTTON , p ) )
     {
       lightsAuto();
       drawMenuButtons();
     }
     else if ( buttonPressed( sliderArea , p ) )
     {
-      struct tftPoint_t touchedLocation;
-      touchedLocation = location(  p  );
+      tftPoint_t touchedLocation;
+
+      touchedLocation = mapToTft( p.x, p.y );
       tftBrightness = map( touchedLocation.y , SLIDER_YPOS, SLIDER_HEIGHT + SLIDER_YPOS, 100, 0 );
       drawBacklightSlider();
     }
@@ -161,16 +162,28 @@ void showStatus()
   const uint16_t BARS_BOTTOM      = 190;
   const uint16_t BARS_HEIGHT      = BARS_BOTTOM;
   const uint16_t BARS_BORDER      = 10;
-  const uint16_t BARS_WIDTH       = 200 / 5;
+  const uint16_t BARS_WIDTH       = 200 / 5; /* note: total width is 200 px */
   const float    HEIGHT_FACTOR    = BARS_HEIGHT / 100.0;
+
+  button_t TFT_CLOCK_AREA
+  {
+    10, 205, 110, 30
+  };
+
+  button_t TFT_NETWORK_AREA
+  {
+    140, 205, 170, 30
+  };
 
   uint16_t channelColor565[NUMBER_OF_CHANNELS];
 
   if ( tftClearScreen )
   {
     tft.fillScreen( ILI9341_BLACK );
-    tftClearScreen = false;
     drawButton( MENU_BUTTON, ILI9341_BLUE, 0 );
+    //tft.drawRect( TFT_CLOCK_AREA.x, TFT_CLOCK_AREA.y, TFT_CLOCK_AREA.w, TFT_CLOCK_AREA.h, ILI9341_WHITE );
+    //tft.drawRect( TFT_NETWORK_AREA.x, TFT_NETWORK_AREA.y, TFT_NETWORK_AREA.w, TFT_NETWORK_AREA.h, ILI9341_WHITE );
+    tftClearScreen = false;
   }
 
   tft.startWrite();
@@ -213,7 +226,7 @@ void showStatus()
     char content[8];
     if ( TFT_SHOW_RAW )
     {
-      snprintf( content, sizeof( content ), " 0x%04X", ledcRead( channelNumber ) );
+      snprintf( content, sizeof( content ), "%04X", ledcRead( channelNumber ) );
     }
     else
     {
@@ -235,23 +248,30 @@ void showStatus()
     tft.setTextColor( TFT_TEMP_COLOR , TFT_BACK_COLOR );
     for ( uint8_t thisSensor = 0; thisSensor < numberOfFoundSensors; thisSensor++ )
     {
-      tft.setCursor( 200, 40 + thisSensor * 30 );
       button_t tempArea;
-      tempArea.x = 220;
-      tempArea.y = 60 + thisSensor * 50;
-      tempArea.w = TFT_BUTTON_WIDTH - 20;
-      tempArea.h = 40;
-      snprintf( tempArea.text, sizeof( tempArea.text ), "%.1f%c", sensor[thisSensor].tempCelcius, char(247) );
 
+      tempArea.x = 220;
+      tempArea.y = 80 + thisSensor * 40;
+      tempArea.w = TFT_BUTTON_WIDTH - 20;
+      tempArea.h = 30;
+      snprintf( tempArea.text, sizeof( tempArea.text ), "%.1f%c", sensor[thisSensor].tempCelcius, char(247) );
       drawButton( tempArea, 0, ILI9341_GREEN );
     }
   }
 
   struct tm timeinfo;
+
   getLocalTime( &timeinfo );
-  tft.setCursor( 18, BARS_BOTTOM + 15 );
-  tft.setTextColor( TFT_DATE_COLOR , TFT_BACK_COLOR );
-  tft.print( asctime( &timeinfo ) );
+
+  strftime( TFT_CLOCK_AREA.text, sizeof( TFT_CLOCK_AREA.text ), "%T", &timeinfo );
+  drawButton( TFT_CLOCK_AREA, 0, 0 );
+
+  tcpip_adapter_ip_info_t ip_info;
+
+  ESP_ERROR_CHECK( tcpip_adapter_get_ip_info( TCPIP_ADAPTER_IF_STA, &ip_info ) );
+  snprintf( TFT_NETWORK_AREA.text, sizeof( TFT_NETWORK_AREA.text ), "%s", ip4addr_ntoa( &ip_info.ip ) );
+  drawButton( TFT_NETWORK_AREA, 0, 0 );
+
 
   if ( touch.touched() )
   {
@@ -266,6 +286,7 @@ void showStatus()
 
 static inline __attribute__((always_inline)) void drawButton( struct button_t button, uint16_t color, uint16_t bordercolor )
 {
+  tft.setTextSize( 2 );
   tft.setTextColor( ILI9341_YELLOW, color );
   if ( color )
   {
@@ -285,9 +306,9 @@ static inline __attribute__((always_inline)) void drawButton( struct button_t bu
 
 static inline __attribute__((always_inline)) void drawMenuButtons()
 {
-  drawButton( LIGHTON_BUTTON,     lightStatus == LIGHTS_ON  ? ILI9341_RED : ILI9341_BLUE, 0 );
-  drawButton( LIGHTOFF_BUTTON,    lightStatus == LIGHTS_OFF ? ILI9341_RED : ILI9341_BLUE, 0 );
-  drawButton( LIGHTAUTO_BUTTON,   lightStatus == LIGHTS_AUTO ? ILI9341_RED : ILI9341_BLUE, 0 );
+  drawButton( LIGHTSON_BUTTON,     lightStatus == LIGHTS_ON  ? ILI9341_RED : ILI9341_BLUE, 0 );
+  drawButton( LIGHTSOFF_BUTTON,    lightStatus == LIGHTS_OFF ? ILI9341_RED : ILI9341_BLUE, 0 );
+  drawButton( LIGHTSAUTO_BUTTON,   lightStatus == LIGHTS_AUTO ? ILI9341_RED : ILI9341_BLUE, 0 );
   drawButton( EXIT_BUTTON, ILI9341_BLUE, 0 );
 }
 
@@ -298,12 +319,7 @@ static inline __attribute__((always_inline)) bool buttonPressed( struct button_t
   return ( clickedLocation.x > button.x && clickedLocation.x < button.x + button.w ) && ( clickedLocation.y > button.y && clickedLocation.y < button.y + button.h );
 }
 
-static inline __attribute__((always_inline)) struct tftPoint_t location( const TS_Point p )
-{
-  return mapToTft( p.x , p.y );
-}
-
-static inline __attribute__((always_inline)) tftPoint_t mapToTft( uint16_t touchX, uint16_t touchY )
+static inline __attribute__((always_inline)) struct tftPoint_t mapToTft( uint16_t touchX, uint16_t touchY )
 {
   uint16_t x, y;
 
@@ -336,7 +352,7 @@ static inline __attribute__((always_inline)) void drawBacklightSlider()
   sliderButton = { SLIDER_XPOS - ( SLIDER_WIDTH / 2 ), ypos, SLIDER_WIDTH, 30 };
 
   snprintf( sliderButton.text, sizeof( sliderButton.text ), "%.0f%%", tftBrightness );
-  drawButton( sliderButton, ILI9341_BLUE, ILI9341_RED );
+  drawButton( sliderButton, ILI9341_BLUE, 0 );
   //set backlight
   ledcWrite( TFT_BACKLIGHT_CHANNEL, map( tftBrightness, 0, 100, 0, backlightMaxvalue ) );
 }

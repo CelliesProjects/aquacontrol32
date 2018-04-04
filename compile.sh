5525113d130addf2083a7f1359c04cf7d2d08aa7
Cellie <m.timmer@wasietsmet.nl>
2018-04-04 09:44:34 +0200

    Improved the compile and flash scripts.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,3 @@
-echo -n 'const char * sketchVersion = "' > gitTagVersion.h
-git --no-pager describe --tags --always --dirty | tr --delete '\n' >>gitTagVersion.h
-echo  -n '";' >> gitTagVersion.h
+echo "const char * sketchVersion = \"$(git describe --tags --always --dirty)\";" > gitTagVersion.h
 ~/arduino-1.8.5/arduino --verify test.ino
 rm gitTagVersion.h
--- flash.sh
+++ flash.sh
@@ -1,5 +1,3 @@
-echo -n 'const char * sketchVersion = "' > gitTagVersion.h
-git --no-pager describe --tags --always --dirty | tr --delete '\n' >>gitTagVersion.h
-echo  -n '";' >> gitTagVersion.h
+echo "const char * sketchVersion = \"$(git describe --tags --always --dirty)\";" > gitTagVersion.h
 ~/arduino-1.8.5/arduino --upload test.ino --pref custom_DebugLevel=esp32_none
 rm gitTagVersion.h

2313f68ccec441b89fa8ddd7cf1e666d153bc5dc
Cellie <aquacontrol@wasietsmet.nl>
2018-04-11 18:20:16 +0200

    Added serial port '/dev/ttyUSB0' to script.

--- flash.sh
+++ flash.sh
@@ -1,3 +1,3 @@
 echo "const char * sketchVersion = \"$(git describe --tags --always)\";" > gitTagVersion.h
-~/arduino-1.8.5/arduino --upload test.ino --pref custom_DebugLevel=esp32_info
+~/arduino-1.8.5/arduino --upload test.ino --pref custom_DebugLevel=esp32_info --port /dev/ttyUSB0
 rm gitTagVersion.h

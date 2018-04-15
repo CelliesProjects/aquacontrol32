a430e85247dcfafc85e2491040b72d30d09ef90b
Cellie <aquacontrol@wasietsmet.nl>
2018-04-15 12:05:27 +0200

    Set debug level to no logging.

--- flash.sh
+++ flash.sh
@@ -1,3 +1,3 @@
 echo "const char * sketchVersion = \"$(git describe --tags --always)\";" > gitTagVersion.h
-~/arduino-1.8.5/arduino --upload test.ino --pref custom_DebugLevel=esp32_info --port /dev/ttyUSB0
+~/arduino-1.8.5/arduino --upload test.ino --pref custom_DebugLevel=esp32_none --port /dev/ttyUSB0
 rm gitTagVersion.h

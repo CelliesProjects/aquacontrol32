440aa514698abe4232fd265bab1079ac01273291
Cellie <aquacontrol@wasietsmet.nl>
2018-05-17 12:32:32 +0200

    Show which version is compiled and flashed.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
+echo Compiling aquacontrol32 version: $(git describe --tags --always)
+echo
 echo "const char * sketchVersion = \"$(git describe --tags --always)\";" > gitTagVersion.h
 ~/arduino-1.8.5/arduino --verify test.ino
 rm gitTagVersion.h
--- flash.sh
+++ flash.sh
@@ -1,3 +1,5 @@
+echo Compiling and flashing aquacontrol32 version: $(git describe --tags --always)
+echo
 echo "const char * sketchVersion = \"$(git describe --tags --always)\";" > gitTagVersion.h
 ~/arduino-1.8.5/arduino --upload test.ino --pref custom_DebugLevel=esp32_none --port /dev/ttyUSB0
 rm gitTagVersion.h

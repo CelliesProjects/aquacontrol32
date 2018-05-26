99b6759d391503e8c8ab99e5ca20060101f5985a
Cellie <aquacontrol@wasietsmet.nl>
2018-05-26 10:29:58 +0200

    Show which branch is compiled.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 echo Compiling aquacontrol32 version: $(git describe --tags --always)
+echo We are on branch: $(git branch | grep \* | cut -d ' ' -f2)
 echo
 echo "const char * sketchVersion = \"$(git describe --tags --always)\";" > gitTagVersion.h
 ~/arduino-1.8.5/arduino --verify test.ino

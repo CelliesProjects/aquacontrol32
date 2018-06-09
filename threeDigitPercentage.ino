0921cccdf50d13e692d3e12654245cb490b853f8
Cellie <CelliesProjects@users.noreply.github.com>
2018-06-09 11:34:20 +0200

    Fixed percentage rounding.

--- threeDigitPercentage.ino
+++ threeDigitPercentage.ino
@@ -4,7 +4,7 @@ static inline __attribute__((always_inline)) void threeDigitPercentage( char *bu
   {
     snprintf( buffer, bufferSize, addPercentSign ? "  0%%  " : "  0  " );
   }
-  else if ( percentage == 100 )
+  else if ( percentage > 99.9 )
   {
     snprintf( buffer, bufferSize, addPercentSign ? " 100%% " : " 100 " );
   }

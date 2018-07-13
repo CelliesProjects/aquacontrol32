7aba9a2ee3b7b4da6122de8626111715d37002b5
Cellie <CelliesProjects@users.noreply.github.com>
2018-07-13 13:20:26 +0200

    Fixed flash script.

--- flash.sh
+++ flash.sh
@@ -2,5 +2,5 @@ echo Compiling and flashing aquacontrol32 version: $(git describe --tags --alway
 echo We are on branch: $(git branch | grep \* | cut -d ' ' -f2)
 echo
 echo "const char * sketchVersion = \"$(git describe --tags --always)\";" > gitTagVersion.h
-~/arduino-1.8.5/arduino --board espressif:esp32:esp32 --upload test.ino --pref custom_DebugLevel=esp32_none --port /dev/ttyUSB0
+~/arduino-1.8.5/arduino --board espressif:esp32:esp32 --upload test.ino --pref custom_DebugLevel=esp32_none --port /dev/ttyUSB0 --pref upload.speed=921600
 rm gitTagVersion.h

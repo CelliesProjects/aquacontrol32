CelliesProjects/aquacontrol32

commit 3f5698e4bd4a8cd0590b72b008c49a3ce1a96958

Changed from (deprecated) asctime to strftime.

webservertask.ino
@@ -114,7 +114,14 @@ void webServerTask ( void * pvParameters )
     AsyncResponseStream *response;
     if ( request->hasArg( "boottime" ) )
     {
-      return request->send( 200, HTML_HEADER, asctime( localtime( &systemStart.tv_sec ) ) );
+      time_t rawtime;
+      struct tm * timeinfo;
+      char buffer [25];
+
+      time (&rawtime);
+      timeinfo = localtime ( &rawtime );
+      strftime ( buffer, sizeof(buffer), "%c", timeinfo );
+      return request->send( 200, HTML_HEADER, buffer );
     }
 
     else if ( request->hasArg( "channelcolors" ) )

commit 9cb8656a1a18a464903db198293831fc3386b45c

Fixed the bug introduced in 3f5698e4bd4a8cd0590b72b008c49a3ce1a96958.

webservertask.ino
@@ -116,14 +116,10 @@ void webServerTask ( void * pvParameters )
     AsyncResponseStream *response;
     if ( request->hasArg( "boottime" ) )
     {
-      time_t rawtime;
-      struct tm * timeinfo;
-      char buffer [25];
+      char response[25];
 
-      time (&rawtime);
-      timeinfo = localtime ( &rawtime );
-      strftime ( buffer, sizeof(buffer), "%c", timeinfo );
-      return request->send( 200, HTML_HEADER, buffer );
+      strftime ( response, sizeof( response ), "%c", localtime ( &systemStart.tv_sec ) );
+      return request->send( 200, HTML_HEADER, response );
     }
 
     else if ( request->hasArg( "channelcolors" ) )
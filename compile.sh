4804e2774f0d1aeac921b5b636a29106c5c886fe
Cellie <CelliesProjects@users.noreply.github.com>
2018-12-28 22:37:23 +0100

    Arduino IDE to 1.8.8.

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@ echo Compiling aquacontrol32 version: $(git describe --tags --always)
 echo We are on branch: $(git branch | grep \* | cut -d ' ' -f2)
 echo
 echo "const char * sketchVersion = \"$(git describe --tags --always)\";" > gitTagVersion.h
-~/arduino-1.8.7/arduino --board espressif:esp32:mhetesp32minikit --verify test.ino --pref custom_DebugLevel=esp32_none --port /dev/ttyUSB0 --pref upload.speed=921600 --pref build.path=temp --preserve-temp-files
+~/arduino-1.8.8/arduino --board espressif:esp32:mhetesp32minikit --verify test.ino --pref custom_DebugLevel=esp32_none --port /dev/ttyUSB0 --pref upload.speed=921600 --pref build.path=temp --preserve-temp-files
 rm gitTagVersion.h

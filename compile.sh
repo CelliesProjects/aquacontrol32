8f790336ff368f099b5c2c0ddc4037deeffb7c78
Cellie <CelliesProjects@users.noreply.github.com>
2019-05-27 16:05:23 +0200

    Flash and compile script cleaned.

--- compile.sh
+++ compile.sh
@@ -1,12 +1,11 @@
-echo Compiling aquacontrol32 version: $(git describe --tags --always)
-echo Branch: $(git branch | grep \* | cut -d ' ' -f2)
-echo
+currentVersion=""
 if [ $(git branch | grep \* | cut -d ' ' -f2) == "master" ]
 then
-  echo "const char * sketchVersion = \"$(git describe --tags --always)\";" > gitTagVersion.h
+  currentVersion=$(git describe --tags --always)
 else
-  echo "const char * sketchVersion = \"$(git describe --tags --always):$(git branch | grep \* | cut -d ' ' -f2)\";" > gitTagVersion.h
+  currentVersion=$(git branch | grep \* | cut -d ' ' -f2):$(git rev-parse --short HEAD)
 fi
-echo $(git describe --tags --always):$(git branch | grep \* | cut -d ' ' -f2)
+echo "const char * sketchVersion = \"$currentVersion\";" > gitTagVersion.h
+echo Compiling aquacontrol32 version: $currentVersion
 ~/arduino-1.8.9/arduino --board espressif:esp32:mhetesp32minikit --verify test.ino --pref custom_DebugLevel=esp32_none --port /dev/ttyUSB0 --pref upload.speed=921600 --pref build.path=temp --preserve-temp-files --pref build.partition=default_ffat
 rm gitTagVersion.h

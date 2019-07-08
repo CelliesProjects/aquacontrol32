be771104e60e91b6881d0917502ec64cdad37afa
Cellie <CelliesProjects@users.noreply.github.com>
2019-07-08 15:32:45 +0200

    Update the scripts.

--- flash.sh
+++ flash.sh
@@ -7,5 +7,5 @@ else
 fi
 echo "const char * sketchVersion = \"$currentVersion\";" > gitTagVersion.h
 echo Compiling and flashing aquacontrol32 version: $currentVersion
-~/arduino-1.8.9/arduino --board espressif:esp32:mhetesp32minikit --upload test.ino --pref custom_DebugLevel=esp32_none --port /dev/ttyUSB0 --pref upload.speed=921600 --preserve-temp-files --pref build.path=temp --pref build.partition=default_ffat --pref build.flash_freq=80m -v | grep  -e 'Using core' -e 'Using board' -e ' uses ' -e 'Using library' -e 'Compiling' -e 'Global'
+~/arduino-1.8.9/arduino --board espressif:esp32:mhetesp32minikit --pref custom_DebugLevel=esp32_none --port /dev/ttyUSB0 --pref upload.speed=921600 --preserve-temp-files --pref build.path=temp --pref build.partition=default_ffat --pref build.flash_freq=80m -v --upload aquacontrol32.ino | grep  -e 'Using core' -e 'Using board' -e ' uses ' -e 'Using library' -e 'Compiling' -e 'Global'
 rm gitTagVersion.h

b9ef7f96ee6f9299659f54a80122e022ef1a5830
Cellie <CelliesProjects@users.noreply.github.com>
2019-07-09 09:58:15 +0200

    Colored output.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 if [ $(grep GIT_TAG deviceSetup.h | grep -ic TRUE) != "1" ]
 then
-  echo ERROR! GIT_TAG not set to true in deviceSetup.h.
+  echo -e '\e[31m'ERROR'\e[0m' GIT_TAG not set to true in deviceSetup.h.
   exit 1
 fi
 currentVersion=""
@@ -11,6 +11,6 @@ else
   currentVersion=$(git branch | grep \* | cut -d ' ' -f2):$(git rev-parse --short HEAD)
 fi
 echo "const char * sketchVersion = \"$currentVersion\";" > gitTagVersion.h
-echo Compiling aquacontrol32 version: $currentVersion
+echo -e Compiling aquacontrol32 version: '\e[36m'$currentVersion'\e[0m'
 ~/arduino-1.8.9/arduino --board espressif:esp32:mhetesp32minikit --pref custom_DebugLevel=esp32_none --port /dev/ttyUSB0 --pref upload.speed=921600 --preserve-temp-files --pref build.path=temp --pref build.partition=default_ffat --pref build.flash_freq=80m -v --verify aquacontrol32.ino | grep  -e 'Using core' -e 'Using board' -e ' uses ' -e 'Using library' -e 'Compiling' -e 'Global'
 rm gitTagVersion.h

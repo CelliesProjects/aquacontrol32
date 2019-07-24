548dae3ba2a18a5c825df5d80f00786f3ad2bb75
Cellie <CelliesProjects@users.noreply.github.com>
2019-07-24 12:45:17 +0200

    Reset reason formatting fixed.

--- ntptask.ino
+++ ntptask.ino
@@ -21,7 +21,7 @@ void ntpTask( void * pvParameters )
   /* save reset reason */
   if ( preferences.getString("bootlog").equalsIgnoreCase("on") ) {
     char content[100];
-    snprintf( content, sizeof( content ), "%s,%s", resetString( 0 ), resetString( 1 ) );
+    snprintf( content, sizeof( content ), " %s,%s", resetString( 0 ), resetString( 1 ) );
     logger.appendToFile( "/reset_reasons.txt", HUMAN_TIME, content );
   }
 

b31d4cd3403d00b0778adc3dd01551d6f5ca89bb
Cellie <CelliesProjects@users.noreply.github.com>
2021-03-22 20:10:31 +0100

    Update to v1.6.8.

--- aquacontrol32.ino
+++ aquacontrol32.ino
@@ -25,7 +25,7 @@ const char * wifi_password = "";                         /* Or use https://githu
 
 #define SET_STATIC_IP              false                 /* If SET_STATIC_IP is set to true then STATIC_IP, GATEWAY, SUBNET and PRIMARY_DNS have to be set to some sane values */
 
-const IPAddress STATIC_IP(192, 168, 0, 60);              /* This should be outside your router dhcp range! */
+const IPAddress STATIC_IP(192, 168, 0, 80);              /* This should be outside your router dhcp range! */
 const IPAddress GATEWAY(192, 168, 0, 1);                 /* Set to your gateway ip address */
 const IPAddress SUBNET(255, 255, 255, 0);                /* Usually 255,255,255,0 but check in your router or pc connected to the same network */
 const IPAddress PRIMARY_DNS(192, 168, 0, 30);            /* Check in your router */
@@ -37,7 +37,7 @@ const IPAddress SECONDARY_DNS( 192, 168, 0, 50 );        /* Check in your router
 #if GIT_TAG
 #include "gitTagVersion.h"
 #else
-const char * sketchVersion = "v1.6.71-master";
+const char * sketchVersion = "v1.6.8";
 #endif
 
 /**************************************************************************
